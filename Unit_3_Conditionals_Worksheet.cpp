0d2efda8936cb78d5b2146e1870b5df5f2e1e6e9
rohsaurus <55811427+rohsaurus@users.noreply.github.com>
2020-11-11 11:57:50 -0500

    Started working on question 5 on if else worksheet

--- Unit_3_Conditionals_Worksheet.cpp
+++ Unit_3_Conditionals_Worksheet.cpp
@@ -2,7 +2,9 @@
  *Rohan Parikh
  *10 November 2020 -
  *Unit 3 Conditionals Worksheets
- *Extra Thing for IF worksheet:
+ *Extra Thing for IF worksheet: For question 2, added be careful message for if user picks 9
+ *Extra Thing for If Else Worksheets: For question 4, gives user choice if they want to reuse their age or pick a new one
+ *
  */
 
 #include <iomanip>
@@ -41,6 +43,10 @@ void ifWorksheets()
     {
         cout << "Lucky Number!\n";
     }
+    else if (number == 9)
+    {
+        cout << "Be careful not to get eaten by 7!\n";
+    }
     else
     {
         cout << "Yep, a plain old number between 5 and 10\n";
@@ -67,6 +73,79 @@ void ifElseWorksheets()
     }
 
     // Question 2
+    cout << "Enter a positive integer\n";
+    int integer;
+    cin >> integer;
+    int remander = integer % 2;
+    if (remander == 0 )
+    {
+        cout << "Your number is even!\n";
+    }
+    else
+    {
+        "Your number is odd\n";
+    }
+
+    // Question 3
+     cout << "How many words do you want to send in your telegram message.\n";
+    int words;
+    cin >> words;
+    if (words <= 15)
+    {
+        cout << "The cost to use the telegram is $8.50.\n";
+    }
+    else
+    {
+        double additionalWords = words - 15;
+        double additionalCost = additionalWords * .25;
+        double totalcost = additionalCost + 8.50;
+        setprecision(2);
+        cout << "Your total cost for the telegram is $" << totalcost << fixed << ".\n";
+    }
+
+    // Question 4
+    cout << "Would you like to enter a new age, or use the one you already entered? Enter new or old. New is for choosing a new age and old if sticking with the one provided earlier\
+    n";
+    string choice;
+    cin >> choice;
+    int birthYear;
+    cout << "Enter the current year\n";
+    int currentYear;
+    cin >> currentYear;
+    if (choice == "new")
+    {
+        cout << "Enter your birthyear\n";
+        cin >> birthYear;
+    }
+    else
+    {
+     birthYear = currentYear - age;
+    }
+    if (birthYear < 1970)
+    {
+        cout << "You qualify for the Juke Box Rally.\n";
+    }
+    cout << "Enter your favorite fruit\n";
+    string fruit;
+    cin >> fruit;
+
+    if (fruit == "Strawberry")
+    {
+        cout << "Strawberry Fields Forever!\n";
+    }
+    else
+    {
+        cout << "Bye,bye Miss American Pie\n";
+    }
+
+    // Question 5
+
+    cout << "Enter two positive integers\n";
+    int numberOne;
+    int numberTwo;
+    cin >> numberOne >> numberTwo;
+    // Need to finish question 5
+    
 }
 
 int main ()
